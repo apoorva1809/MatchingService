506f7b1ddd2702f7a768fbf06fd6bcc3c9621f3d
apoorvatejavanam <apoorvatejavanam@10.0.0.77>
2019-05-27 21:09:29 -0400

    Made changes to build.gradle

--- src/main/java/com/matchingservices/oracle/dao/impl/MatchingServiceOracleDaoImpl.java
+++ src/main/java/com/matchingservices/oracle/dao/impl/MatchingServiceOracleDaoImpl.java
@@ -15,7 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.mongodb.core.MongoOperations;
+//import org.springframework.data.mongodb.core.MongoOperations;
 //import org.springframework.data.mongodb.core.query.Query;
 //import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Repository;
